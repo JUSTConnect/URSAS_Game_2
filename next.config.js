fa574a81dd0c4b8d6e035d37d260238244303661
Ilkhan <ilhanchanel20002@gmail.com>
2023-03-06 15:18:21 +0300

    .

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-// next.config.js
-module.exports = {
-  // ... rest of the configuration.
   output: 'standalone',
 }
 
