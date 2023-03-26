984c0f6d3aa1e51dcb5ed16b264a7860f5455de0
Ilkhan <ilhanchanel20002@gmail.com>
2023-03-26 22:25:47 +0300

    .

--- src/components/Header/Dropdown/index.tsx
+++ src/components/Header/Dropdown/index.tsx
@@ -54,7 +54,7 @@ const Dropdown = (props: DropdownProps) => {
             <div className={ [css.text, 'fixMargin'].join(' ') }>
                 { props.text }       
             </div>
-            <div className={ css.badge}><span className={ css.badgeValue }>{ props.badgeValue }</span></div>
+            <div className={ css.badge}><span className={ [css.badgeValue, 'fixMargin'].join(' ') }>{ props.badgeValue }</span></div>
             <svg className={ [css.arrowIcon, isActive() ? css.arrowIconActive : '' ].join(' ') } viewBox="0 0 8 6" xmlns="http://www.w3.org/2000/svg">
                 <path d="M4.43301 5.25C4.24056 5.58333 3.75944 5.58333 3.56699 5.25L0.968911 0.75C0.776461 0.416667 1.01702 6.10471e-07 1.40192 5.76822e-07L6.59808 1.2256e-07C6.98298 8.8911e-08 7.22354 0.416667 7.03109 0.75L4.43301 5.25Z" fill="#B5C4E3"/>
             </svg>
