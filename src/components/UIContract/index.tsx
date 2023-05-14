81a423b28444ae4a52c4c033c77dede65424f723
Ilkhan <ilhanchanel20002@gmail.com>
2023-05-15 00:18:54 +0300

    .

--- src/components/UIContract/index.tsx
+++ src/components/UIContract/index.tsx
@@ -3,8 +3,9 @@ import css from './index.module.scss'
 import Text from '@components/Typography'
 import { Variant as text } from '@components/Typography'
 import ABI from '@contract/abi'
+import ABIGame from '@contract/abi-game'
 import { ABIItem } from '@/types/web3'
-import { getMintContract } from '@/utils/web3'
+import { getMintContract, getGameContract } from '@/utils/web3'
 
 import Card from './CardMethod'
 
@@ -27,6 +28,32 @@ export default () => {
         <div className={ css.section }>
             <div className={ css.cardsMethod }>
                 { ABI.map((item) =>
+                    item.type === 'error' &&
+                        <div key={ item.name } className={ css.cardMethod }>
+                            { item.name }
+                        </div>
+                ) }
+            </div>
+        </div>
+        <br />
+        <br />
+        <Text variant={text.h1}>Game Contract ABI</Text>  
+        <Text variant={text.h2}>Functions ({ ABIGame.length })</Text>  
+        <div className={ css.section }>
+            <div className={ css.cardsMethod }>
+                { (ABIGame as ABIItem[]).map((item: ABIItem, index) =>
+                    <Card
+                        item={item}
+                        getContract={getGameContract}
+                        key={index}
+                    />
+                ) }
+            </div>
+        </div>
+        <Text variant={text.h2}>Errors</Text>  
+        <div className={ css.section }>
+            <div className={ css.cardsMethod }>
+                { ABIGame.map((item) =>
                     item.type === 'error' &&
                         <div key={ item.name } className={ css.cardMethod }>
                             { item.name }
